58b1e75dde958c0b8c3cae0f061a8e8f4845d5d7
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-04-05 17:11:19 -0400

    created header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Header.css";
+import { Container, Row, Col, Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+
+function Header() {
+    return (
+      <header>
+        <Navbar
+          className="Navbar"
+          bg="dark"
+          variant="dark"
+          expand="lg"
+          collapseOnSelect
+        >
+          <Container>
+            <Navbar.Brand href="/">OneStopShop</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                <Nav.Link href="/cart"><i className="fas fa-shopping-cart mr-1"></i>  Cart</Nav.Link>
+                <Nav.Link href="login"><i className="fas fa-user mr-1"></i>  Sign In</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    );
+}
+
+export default Header
